25f0bb616adcd1b7be201d00d6751ed03681878b
Boba fousseiny Kamate <bobafousseiny.kamate@etu.uae.ac.ma>
2025-05-15 19:43:35 +0100

    Update vite.config.ts
    deployement config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -8,4 +8,15 @@ export default defineConfig({
     host:"0.0.0.0"
   },
   plugins: [react()],
-})
+})*/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+});
+
